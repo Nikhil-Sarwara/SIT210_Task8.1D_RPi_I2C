4a8ab62840116a96b5d1af94d7af9f91fb333b42
Nikhil-Sarwara <117506262+Nikhil-Sarwara@users.noreply.github.com>
2023-09-30 12:00:00 +0530

    Update i2c_task.py

--- i2c_task.py
+++ i2c_task.py
@@ -1,16 +1,19 @@
-# SPDX-FileCopyrightText: 2020 Bryan Siepert, written for Adafruit Industries
-
-# SPDX-License-Identifier: Unlicense
 import time
 import board
 import adafruit_bh1750
 
-i2c = board.I2C()  # uses board.SCL and board.SDA
-# i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
+# Create an I2C object using the default pins
+i2c = board.I2C() 
+
+# Create a BH1750 sensor object using the I2C object
 sensor = adafruit_bh1750.BH1750(i2c)
 
+# Continuously read the light intensity and print it to the console
 while True:
+    # Read the light intensity in lux
     data = sensor.lux
+
+    # Print a message to the console depending on the light intensity
     if data < 20:
         print("Too dark")
     elif data < 50:
@@ -22,5 +25,6 @@ while True:
     else:
         print("Too bright")
 
+    # Wait for 1 second before reading the light intensity again
     time.sleep(1)
-
+    
